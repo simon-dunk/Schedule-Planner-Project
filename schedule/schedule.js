28c3cbacd251571bf2ec8d90180c68023655b074
simon <simon.dunk@okbu.edu>
2023-11-06 01:10:05 -0600

    changes

--- schedule/schedule.js
+++ schedule/schedule.js
@@ -46,18 +46,15 @@ const dataObj = {
     }
 };
 
+let globalIndex = 0;
+
 function styleTables(table) {
-    // Assuming you want the cell index of the second row (index 1) and third cell (index 2)
     const numberOfCells = table.rows[0].cells.length;
     const numberOfRows = table.rows.length;
-    console.log(`The number of rows is ${numberOfRows}`);
-    console.log(`The number of cells is ${numberOfCells}`);
-
 
     for (let i = 0; i < numberOfRows; i++) {
         console.log(i);
         if (((i + 1) % 2) > 0) {
-            // console.log("parsed table index: " + i)
             table.rows[i].style.backgroundColor = "rgb(230, 230, 230)";
         }
     }
@@ -124,8 +121,6 @@ function isValidTimeFormat(input) {
     // sets patters for time of day format hh:mm and ranges of times
     const timePattern = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
     const timeRangePattern = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]\s*-\s*([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
-
-    // const trimmedInput = input.trim();
     const trimmedInput = input.replace(/[^0-9:-]/g, '');
 
     if (timeRangePattern.test(trimmedInput)) {
@@ -260,7 +255,6 @@ function formatTimeRange(inputString) {
         return `${formattedHours}:${minutes}`;
     }
 
-    // Format both time parts and join them with " - "
     const formattedTimeRange = timeParts.map(formatTimePart).join(' - ');
 
     return formattedTimeRange;
@@ -271,7 +265,6 @@ function formatTimeRange(inputString) {
 const table = document.getElementById("editableTable");
 table.addEventListener('input', function (event) {
     const target = event.target;
-    // const selection = window.getSelection();
     const output = target.textContent;
     const outputID = target.id;
     const targetParentNode = target.parentNode;
@@ -280,13 +273,14 @@ table.addEventListener('input', function (event) {
 
     console.log(`Row '${targetGrandparentNodeId}' selected `);
     console.log(`Cell contains '${target.textContent}'`);
-    // console.log("ID: " + outputID);
+
 
     if (outputID) {
         parseData(output, outputID, targetGrandparentNodeId);
     }
 });
-let globalIndex = 0;
+
+
 table.addEventListener('paste', (event) => {
     const target = event.target;
 
@@ -363,10 +357,6 @@ convert.addEventListener('paste', function (event) {
     }
 
     const time = formatTimeRange(values[9]);
-    // let time = values[9].replace(/[^0-9:]/g, '');
-    // time = time.replace(/\b0(\d)/g, '$1');
-    // time = time.replace(/(\d+:\d+)(\d+)/, '$1 - $2');
-
     const prof = values[19].split(" ")[2];
     const location = values[21];
     let days = "";
@@ -388,6 +378,7 @@ convert.addEventListener('paste', function (event) {
     }
 
 
+    // Adds the specific data to each individual cell (Easier than iteration)
     const cells = table.querySelectorAll('div');
     ++globalIndex
     cells[globalIndex].innerHTML = crn;
